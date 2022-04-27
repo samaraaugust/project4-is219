badda1ab0df43a1cf08e996a331697b43abe51f5
samaraaugust <samaraaugust123@gmail.com>
2022-04-27 13:39:56 -0400

    added folder for simple pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 """A simple flask web app"""
 import os
-from flask import Flask
+from flask import Flask, render_template
 from app.cli import create_database
 from app.db import db
 from app.db.models import User
-
+import flask_login
+from flask_bootstrap import Bootstrap5
+login_manager = flask_login.LoginManager()
 
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -12,8 +14,10 @@ def page_not_found(e):
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
-
+    bootstrap = Bootstrap5(app)
     app.register_error_handler(404, page_not_found)
     db_dir = "database/db.sqlite"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
@@ -22,8 +26,13 @@ def create_app():
     # add command function to cli commands
     app.cli.add_command(create_database)
 
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+
 
     return app
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
