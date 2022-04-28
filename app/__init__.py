2a739d76f4535e009a224264175c36ed35737be9
samaraaugust <samaraaugust123@gmail.com>
2022-04-28 00:50:34 -0400

    added folder for context processors

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_bootstrap import Bootstrap5
 login_manager = flask_login.LoginManager()
 from app.simple_pages import simple_pages
 from app.auth import auth
+from app.context_processors import utility_text_processors
 
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -20,6 +21,7 @@ def create_app():
     login_manager.login_view = "auth.login"
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
     bootstrap = Bootstrap5(app)
+    app.context_processor(utility_text_processors)
     app.register_error_handler(404, page_not_found)
     db_dir = "database/db.sqlite"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
