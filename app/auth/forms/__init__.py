6e8b164fd722ba19f966b3a78a471284e7487d39
samaraaugust <samaraaugust123@gmail.com>
2022-05-03 23:02:53 -0400

    added password constraint for login and register

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -6,11 +6,13 @@ from wtforms.fields import *
 class login_form(FlaskForm):
     email = EmailField('Email Address', [
         validators.DataRequired(),
+        validators.Email()
     ])
 
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.length(min=6, max=35)
+        validators.length(min=6, max=35),
+        validators.regexp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,})', message="Invalid Password")
     ])
     submit = SubmitField()
 
@@ -18,13 +20,14 @@ class login_form(FlaskForm):
 class register_form(FlaskForm):
     email = EmailField('Email Address', [
         validators.DataRequired(),
-
+        validators.Email()
     ], description="You need to signup with an email")
 
     password = PasswordField('Create Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match'),
-
+        validators.length(min=6, max=35),
+        validators.regexp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,})', message="Invalid Password")
     ], description="Create a password ")
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
     submit = SubmitField()
