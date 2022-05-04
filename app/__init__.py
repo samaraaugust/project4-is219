be1621b4076c362cff8ff6a39e0ce711783ef2e3
samaraaugust <samaraaugust123@gmail.com>
2022-05-03 22:44:56 -0400

    added  config file and auto db folder

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 import os
 from flask import Flask, render_template
 from app.cli import create_database
-from app.db import db
+from app.db import db, database
 from app.db.models import User
 import flask_login
 from flask_bootstrap import Bootstrap5
@@ -22,16 +22,24 @@ def page_not_found(e):
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+    app = Flask(__name__)
+    if os.environ.get("FLASK_ENV") == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif os.environ.get("FLASK_ENV") == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif os.environ.get("FLASK_ENV") == "testing":
+        app.config.from_object("app.config.TestingConfig")
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    # app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
     bootstrap = Bootstrap5(app)
     app.context_processor(utility_text_processors)
     app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # db_dir = "database/db.sqlite"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
+    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.register_blueprint(simple_pages)
+    app.register_blueprint(database)
     app.register_blueprint(auth)
     app.register_blueprint(recipes)
     db.init_app(app)
