103640ef3d5545e5fb551a63b396615c61841f64
samaraaugust <samaraaugust123@gmail.com>
2022-05-04 00:24:11 -0400

    added method to edit recipe

--- app/recipes/forms/__init__.py
+++ app/recipes/forms/__init__.py
@@ -8,3 +8,10 @@ class new_recipe(FlaskForm):
     ingredients = TextAreaField('Ingredients', [validators.length(min=1, max=300)])
     image = FileField('Image')
     submit = SubmitField()
+
+class edit_recipe(FlaskForm):
+    title = TextAreaField('Title', [validators.length(min=1, max=300)])
+    description = TextAreaField('Description', [validators.length(min=1, max=300)])
+    ingredients = TextAreaField('Ingredients', [validators.length(min=1, max=300)])
+    image = FileField('Image')
+    submit = SubmitField()
